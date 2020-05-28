16fdfc613c03d52a55cdc0c50169d22ebaeb096f
Jiahuan Yang <jiahuan.yang@thoughtworks.com>
2020-05-28 18:50:28 +0800

    feat: complete blank quiz homework

--- src/main/java/exam/paperContext/userInterface/PaperDTO.java
+++ src/main/java/exam/paperContext/userInterface/PaperDTO.java
@@ -1,14 +1,13 @@
 package exam.paperContext.userInterface;
 
-import exam.paperContext.domain.model.paper.Paper;
 import exam.paperContext.domain.model.paper.PaperId;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class PaperDTO {
+
     private String uri;
 
     public static PaperDTO from(PaperId paperId) {
